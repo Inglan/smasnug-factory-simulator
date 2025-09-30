dc8efc3d87f63f7d9bf9c540cd7cf7c1a5d11f2a
Ingo Wolf <me@ingowolf.au>
2025-09-30 16:14:16 +1000

    start working on ui

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,8 @@
     import "../app.css";
     import { ModeWatcher } from "mode-watcher";
     import favicon from "$lib/assets/favicon.svg";
+    import { state } from "$lib/state.svelte";
+    import { Button } from "$lib/components/ui/button";
 
     let { children } = $props();
 </script>
@@ -11,4 +13,19 @@
 </svelte:head>
 
 <ModeWatcher />
-{@render children?.()}
+
+<div class="container mx-auto flex flex-col gap-4">
+    <div class="flex flex-row w-full pt-4 gap-4">
+        <h1 class="text-4xl">Smasnug Factory Simulator</h1>
+        <div class="grow"></div>
+        <div><span class="text-4xl">{$state.money}</span> Smascoins</div>
+        <div><span class="text-4xl">{$state.currentDay}</span> Days</div>
+    </div>
+    <div class="flex flex-row w-full gap-2">
+        <Button variant="default">Products</Button>
+        <Button variant="ghost">Advertising</Button>
+        <Button variant="ghost">Expansion</Button>
+        <Button variant="ghost">Analytics</Button>
+    </div>
+    {@render children?.()}
+</div>
