1e0b52ded3162691f8d6a4a3101d89dffe08fe49
Ingo Wolf <me@ingowolf.au>
2025-09-30 16:28:58 +1000

    add product types

---
+++ src/lib/constants.ts
@@ -0,0 +1,38 @@
+export const ProductTypes = {
+  phone: {
+    cost: 300,
+    baseDemandPerDay: 100,
+    productionPerFactoryPerDay: 50,
+    costToStart: 1000,
+  },
+  tablet: {
+    cost: 400,
+    baseDemandPerDay: 20,
+    productionPerFactoryPerDay: 10,
+    costToStart: 100000,
+  },
+  tv: {
+    cost: 1000,
+    baseDemandPerDay: 5,
+    productionPerFactoryPerDay: 2,
+    costToStart: 1000000,
+  },
+  earbuds: {
+    cost: 100,
+    baseDemandPerDay: 20,
+    productionPerFactoryPerDay: 10,
+    costToStart: 10000,
+  },
+  smartwatch: {
+    cost: 200,
+    baseDemandPerDay: 20,
+    productionPerFactoryPerDay: 10,
+    costToStart: 10000,
+  },
+  laptop: {
+    cost: 800,
+    baseDemandPerDay: 10,
+    productionPerFactoryPerDay: 5,
+    costToStart: 100000,
+  },
+};
