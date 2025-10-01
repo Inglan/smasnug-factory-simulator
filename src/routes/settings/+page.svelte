01d41a37acf0b74265808eac0065c84c00c97774
Ingo Wolf <me@ingowolf.au>
2025-10-01 16:39:50 +1000

    add settings

--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -1,1 +1,14 @@
+<script>
+    import { goto } from "$app/navigation";
+    import Button from "$lib/components/ui/button/button.svelte";
+    import { state } from "$lib/state.svelte";
+</script>
+
 <h2 class="text-2xl">Settings</h2>
+
+<Button
+    onclick={() => {
+        state.reset();
+        goto("/");
+    }}>Reset progress</Button
+>
