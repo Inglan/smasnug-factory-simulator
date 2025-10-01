f018a6f6ce3a45a189e6d2ef2c7ee9f32542a82f
Ingo Wolf <me@ingowolf.au>
2025-10-01 13:42:56 +1000

    add products

--- src/routes/products/+page.svelte
+++ src/routes/products/+page.svelte
@@ -7,6 +7,23 @@
     }
 </script>
 
-{#each Object.entries($state.products) as [productId, product]}
-    {getProduct(productId as keyof typeof Products).name}
-{/each}
+<div class="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-4">
+    {#each Object.entries($state.products) as [productId, product]}
+        <div class="flex flex-col bg-card border rounded-md p-4">
+            <h2 class="text-2xl">
+                {getProduct(productId as keyof typeof Products).name}
+            </h2>
+            <h3 class="text-md">
+                Started production: day {product.startedProduction}
+            </h3>
+            <div class="grid grid-cols-2">
+                <div>Selling for ${product.sellingPrice}</div>
+                <div>{product.stock} in stock</div>
+                <div>{product.totalSold} sold</div>
+                <div>{product.totalProduced} produced</div>
+                <div>${product.totalRevenue} revenue</div>
+                <div>${product.totalProfit} profit</div>
+            </div>
+        </div>
+    {/each}
+</div>
