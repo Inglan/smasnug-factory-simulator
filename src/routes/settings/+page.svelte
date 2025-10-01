85ec92d0e7c360b00c5b9de6f8544e6dfce6a0c3
Ingo Wolf <me@ingowolf.au>
2025-10-01 16:42:14 +1000

    show toast on repeat

--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -2,6 +2,7 @@
     import { goto } from "$app/navigation";
     import Button from "$lib/components/ui/button/button.svelte";
     import { state } from "$lib/state.svelte";
+    import { toast } from "svelte-sonner";
 </script>
 
 <h2 class="text-2xl">Settings</h2>
@@ -9,6 +10,7 @@
 <Button
     onclick={() => {
         state.reset();
+        toast.success("Progress reset!");
         goto("/");
     }}>Reset progress</Button
 >
