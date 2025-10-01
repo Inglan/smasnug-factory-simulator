a700e8ebcf2c4313c434d875af2823c11d500aa9
Ingo Wolf <me@ingowolf.au>
2025-10-01 15:26:53 +1000

    add functionality for when production isnt started

--- src/routes/products/+page.svelte
+++ src/routes/products/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
     import { Products } from "$lib/constants";
     import { state } from "$lib/state.svelte";
+    import clsx from "clsx";
 
     function getProduct(productId: keyof typeof Products) {
         return Products[productId];
@@ -9,13 +10,17 @@
 
 <div class="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-4">
     {#each Object.entries($state.products) as [productId, product]}
-        <div class="flex flex-col bg-card border rounded-md p-4">
+        {@const productInfo = getProduct(productId as keyof typeof Products)}
+        <div
+            class={clsx(
+                "flex flex-col bg-card border rounded-md p-4",
+                !product.startedProduction &&
+                    "border-dashed opacity-75 hover:opacity-100 duration-300",
+            )}
+        >
             <h2 class="text-2xl">
-                {getProduct(productId as keyof typeof Products).name}
+                {productInfo.name}
             </h2>
-            <h3 class="text-md">
-                Started production: day {product.startedProduction}
-            </h3>
             <div class="grid grid-cols-2">
                 <div>Selling for ${product.sellingPrice}</div>
                 <div>{product.stock} in stock</div>
