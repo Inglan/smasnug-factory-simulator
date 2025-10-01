64897e63de766fd9ae13c29ff1374e0a87066d8a
Ingo Wolf <me@ingowolf.au>
2025-10-01 13:36:28 +1000

    add displaying products

--- src/routes/products/+page.svelte
+++ src/routes/products/+page.svelte
@@ -1,9 +1,12 @@
-<script>
+<script lang="ts">
     import { Products } from "$lib/constants";
     import { state } from "$lib/state.svelte";
+
+    function getProduct(productId: keyof typeof Products) {
+        return Products[productId];
+    }
 </script>
 
-{#each Object.entries($state.products) as [key, value]}
-    {key}
-    {value}
+{#each Object.entries($state.products) as [productId, product]}
+    {getProduct(productId as keyof typeof Products).name}
 {/each}
