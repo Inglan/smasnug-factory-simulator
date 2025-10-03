d68d72bd3ca04244dd22c341dd9b06e74c661787
Ingo Wolf <me@ingowolf.au>
2025-10-03 10:11:52 +1000

    add money logic

--- src/lib/game/factories.ts
+++ src/lib/game/factories.ts
@@ -1,19 +1,26 @@
+import { FactoryConstants, Products } from "$lib/constants";
 import { state } from "$lib/state.svelte";
 import type { ProductTypes } from "$lib/types";
 import { get } from "svelte/store";
 
 export function buyFactory(type: ProductTypes) {
+  const factoryCost = Products[type].cost * FactoryConstants.baseCostMultiplier;
   const currentState = get(state);
+  if (currentState.money < factoryCost) return;
   let factories = currentState.factories;
   factories.push({
     type: type as ProductTypes,
     efficiency: 1,
     purchaseData: {
       day: currentState.currentDay,
-      cost: 1,
-      value: 1,
+      cost: factoryCost,
+      value: factoryCost,
     },
     totalProduced: 0,
   });
-  state.set({ ...currentState, factories });
+  state.set({
+    ...currentState,
+    factories,
+    money: currentState.money - factoryCost,
+  });
 }
