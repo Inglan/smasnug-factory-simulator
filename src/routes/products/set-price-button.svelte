22a39638f33086fa0fb86ea0917131854852e05a
Ingo Wolf <me@ingowolf.au>
2025-10-03 20:17:07 +1000

    start working on price button

--- src/routes/products/set-price-button.svelte
+++ src/routes/products/set-price-button.svelte
@@ -1,8 +1,47 @@
 <script lang="ts">
-    import Button from "$lib/components/ui/button/button.svelte";
+    import Button, {
+        buttonVariants,
+    } from "$lib/components/ui/button/button.svelte";
     import type { StateProduct } from "$lib/types";
+    import * as Dialog from "$lib/components/ui/dialog/index.js";
+    import Input from "$lib/components/ui/input/input.svelte";
+    import { state } from "$lib/state.svelte";
 
     let { product }: { product: StateProduct } = $props();
+
+    let enteredPrice = $state(0);
 </script>
 
-<Button>Set price</Button>
+<Dialog.Root open={true}>
+    <Dialog.Trigger class={buttonVariants()}>Set price</Dialog.Trigger>
+    <Dialog.Content>
+        <form
+            onsubmit={(e) => {
+                e.preventDefault();
+                if (enteredPrice !== product.sellingPrice) {
+                    // $state.products[product.id].sellingPrice = enteredPrice;
+                    alert(`Price updated to ${enteredPrice}`);
+                }
+            }}
+            class="gap-4 grid"
+        >
+            <Dialog.Header>
+                <Dialog.Title>Set price</Dialog.Title>
+            </Dialog.Header>
+            <div class="grid gap-4">
+                <Input
+                    id="price"
+                    type="number"
+                    bind:value={enteredPrice}
+                    class="col-span-3"
+                />
+            </div>
+            <Dialog.Footer>
+                <Dialog.Close class={buttonVariants({ variant: "outline" })}
+                    >Cancel</Dialog.Close
+                >
+                <Button type="submit">Update</Button>
+            </Dialog.Footer>
+        </form>
+    </Dialog.Content>
+</Dialog.Root>
