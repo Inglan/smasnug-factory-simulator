167a1612e1ecf6aed0f8d72121ba133cb6a7c660
Ingo Wolf <me@ingowolf.au>
2025-10-04 20:18:45 +1000

    add some more randomness and produce using tick

--- src/lib/game/loop.svelte.ts
+++ src/lib/game/loop.svelte.ts
@@ -2,33 +2,41 @@ import { gameState, time } from "$lib/state.svelte";
 import { get } from "svelte/store";
 import { getProductionPerDay } from "./factories";
 import { updated } from "$app/state";
-import { PRODUCTS, TICK } from "$lib/constants";
+import { DAY_LENGTH, PRODUCTS, TICK } from "$lib/constants";
 
 function day() {
   let updatedGameState = get(gameState);
-
-  updatedGameState.factories.forEach((factory, factoryIndex) => {
-    for (let i = 0; i < getProductionPerDay(factory); i++) {
-      if (updatedGameState.money >= PRODUCTS[factory.type].cost) {
-        updatedGameState.products[factory.type].stock += 1;
-        updatedGameState.factories[factoryIndex].totalProduced += 1;
-        updatedGameState.money -= PRODUCTS[factory.type].cost;
-      }
-    }
-  });
-
   updatedGameState.currentDay += 1;
-
   gameState.set(updatedGameState);
 }
 
 export function init() {
   const tickInterval = setInterval(() => {
-    if (time.tick % 300 === 0 && time.tick != 0) {
+    let updatedGameState = get(gameState);
+
+    if (time.tick % DAY_LENGTH === 0 && time.tick != 0) {
       day();
     }
 
+    updatedGameState.factories.forEach((factory, factoryIndex) => {
+      if (
+        time.tick %
+          (DAY_LENGTH / getProductionPerDay(factory) +
+            (Math.round(Math.random() * 4) - 2)) ===
+          0 &&
+        Math.random() < 0.9 &&
+        updatedGameState.money >= PRODUCTS[factory.type].cost
+      ) {
+        updatedGameState.products[factory.type].stock += 1;
+        updatedGameState.products[factory.type].totalProduced += 1;
+        updatedGameState.factories[factoryIndex].totalProduced += 1;
+        updatedGameState.money -= PRODUCTS[factory.type].cost;
+      }
+    });
+
     time.tick += 1;
+
+    gameState.set(updatedGameState);
   }, TICK);
 
   return () => {
