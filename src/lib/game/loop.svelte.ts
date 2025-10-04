16ad3e13eab1203d200a51e48a4bd6e588cce584
Ingo Wolf <me@ingowolf.au>
2025-10-04 20:09:02 +1000

    implement new ticks system

--- src/lib/game/loop.svelte.ts
+++ src/lib/game/loop.svelte.ts
@@ -4,32 +4,34 @@ import { getProductionPerDay } from "./factories";
 import { updated } from "$app/state";
 import { PRODUCTS, TICK } from "$lib/constants";
 
-export function init() {
-  // Day
-  const dayInterval = setInterval(() => {
-    let updatedGameState = get(gameState);
+function day() {
+  let updatedGameState = get(gameState);
 
-    updatedGameState.factories.forEach((factory, factoryIndex) => {
-      for (let i = 0; i < getProductionPerDay(factory); i++) {
-        if (updatedGameState.money >= PRODUCTS[factory.type].cost) {
-          updatedGameState.products[factory.type].stock += 1;
-          updatedGameState.factories[factoryIndex].totalProduced += 1;
-          updatedGameState.money -= PRODUCTS[factory.type].cost;
-        }
+  updatedGameState.factories.forEach((factory, factoryIndex) => {
+    for (let i = 0; i < getProductionPerDay(factory); i++) {
+      if (updatedGameState.money >= PRODUCTS[factory.type].cost) {
+        updatedGameState.products[factory.type].stock += 1;
+        updatedGameState.factories[factoryIndex].totalProduced += 1;
+        updatedGameState.money -= PRODUCTS[factory.type].cost;
       }
-    });
+    }
+  });
 
-    updatedGameState.currentDay += 1;
+  updatedGameState.currentDay += 1;
 
-    gameState.set(updatedGameState);
-  }, 1000 * 30);
+  gameState.set(updatedGameState);
+}
 
+export function init() {
   const tickInterval = setInterval(() => {
+    if (time.tick % 300 === 0 && time.tick != 0) {
+      day();
+    }
+
     time.tick += 1;
   }, TICK);
 
   return () => {
-    clearInterval(dayInterval);
     clearInterval(tickInterval);
   };
 }
