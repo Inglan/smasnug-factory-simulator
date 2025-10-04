ac914f67d7aca51d52e73698b0f2a0cc014d1c5e
Ingo Wolf <me@ingowolf.au>
2025-10-04 17:10:33 +1000

    add production stuff

--- src/lib/game/loop.svelte.ts
+++ src/lib/game/loop.svelte.ts
@@ -1,14 +1,26 @@
 import { gameState } from "$lib/state.svelte";
 import { get } from "svelte/store";
+import { getProductionPerDay } from "./factories";
+import { updated } from "$app/state";
+import { Products } from "$lib/constants";
 
 export function init() {
   // Day
   const dayInterval = setInterval(() => {
-    const currentGameState = get(gameState);
-    gameState.set({
-      ...currentGameState,
-      currentDay: currentGameState.currentDay + 1,
+    let updatedGameState = get(gameState);
+
+    updatedGameState.factories.forEach((factory) => {
+      for (let i = 0; i < getProductionPerDay(factory); i++) {
+        if (updatedGameState.money >= Products[factory.type].cost) {
+          updatedGameState.products[factory.type].stock += 1;
+          updatedGameState.money -= Products[factory.type].cost;
+        }
+      }
     });
+
+    updatedGameState.currentDay += 1;
+
+    gameState.set(updatedGameState);
   }, 1000 * 30);
 
   return () => {
