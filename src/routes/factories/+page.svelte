64e7c16f82a2e7de3e53a9427833a0a88f193d10
Ingo Wolf <me@ingowolf.au>
2025-10-05 13:00:00 +1100

    minor redeisgn to factories page

--- src/routes/factories/+page.svelte
+++ src/routes/factories/+page.svelte
@@ -15,82 +15,86 @@
     import type { ProductTypes } from "$lib/types";
     import { EllipsisVertical, Smartphone } from "@lucide/svelte";
     import clsx from "clsx";
+    import * as Card from "$lib/components/ui/card/index.js";
 </script>
 
 <div class="grid lg:grid-cols-2 grid-cols-1 gap-4">
     {#each $gameState.factories as factory, index}
-        <div
+        <Card.Root
             class={clsx(
-                "flex flex-col bg-card border rounded-md p-4 gap-2",
                 factory.paused &&
                     "border-dashed opacity-75 hover:opacity-100 duration-300 bg-transparent",
             )}
         >
-            <div class="w-full flex flex-row gap-2">
+            <Card.Header class="w-full flex flex-row gap-2">
                 <div class="flex flex-col">
-                    <h2 class="text-2xl">
+                    <Card.Title>
                         {PRODUCTS[factory.type].name}
                         factory ({index + 1})
-                    </h2>
-                    <h3 class="text-md">
+                    </Card.Title>
+                    <Card.Description>
                         Purchased on day {factory.purchaseData.day}
-                    </h3>
+                    </Card.Description>
                 </div>
                 <div class="grow"></div>
-                {#if factory.paused}
-                    <Button
-                        onclick={() => {
-                            factory.paused = !factory.paused;
-                        }}>Resume production</Button
-                    >
-                {:else}
-                    <Button
-                        disabled={getFactoryUpgradeCost(factory) >
-                            $gameState.money ||
-                            factory.level >=
-                                FACTORY_CONSTANTS.upgrades.maxLevel}
-                        onclick={() => {
-                            upgradeFactory(index);
-                        }}
-                        >{factory.level >= FACTORY_CONSTANTS.upgrades.maxLevel
-                            ? "Reached upgrade limit"
-                            : `Upgrade ($${getFactoryUpgradeCost(factory)})`}</Button
-                    >
-                {/if}
-                <DropdownMenu.Root>
-                    <DropdownMenu.Trigger
-                        class={buttonVariants({ size: "icon" })}
-                    >
-                        <EllipsisVertical />
-                    </DropdownMenu.Trigger>
-                    <DropdownMenu.Content>
-                        <DropdownMenu.Group>
-                            <DropdownMenu.Label>Factory</DropdownMenu.Label>
-                            <DropdownMenu.Separator />
-                            <DropdownMenu.Item
-                                onSelect={() => {
-                                    factory.paused = !factory.paused;
-                                }}
-                                >{factory.paused ? "Resume" : "Pause"} production</DropdownMenu.Item
-                            >
-                            <DropdownMenu.Item
-                                onSelect={() => {
-                                    $gameState.factories.splice(index, 1);
-                                    $gameState.money += Math.round(
+                <div class="flex md:flex-row flex-col gap-2">
+                    {#if factory.paused}
+                        <Button
+                            onclick={() => {
+                                factory.paused = !factory.paused;
+                            }}>Resume production</Button
+                        >
+                    {:else}
+                        <Button
+                            disabled={getFactoryUpgradeCost(factory) >
+                                $gameState.money ||
+                                factory.level >=
+                                    FACTORY_CONSTANTS.upgrades.maxLevel}
+                            onclick={() => {
+                                upgradeFactory(index);
+                            }}
+                            >{factory.level >=
+                            FACTORY_CONSTANTS.upgrades.maxLevel
+                                ? "Reached upgrade limit"
+                                : `Upgrade ($${getFactoryUpgradeCost(factory)})`}</Button
+                        >
+                    {/if}
+                    <DropdownMenu.Root>
+                        <DropdownMenu.Trigger
+                            class={buttonVariants({ size: "icon" })}
+                        >
+                            <EllipsisVertical />
+                        </DropdownMenu.Trigger>
+                        <DropdownMenu.Content>
+                            <DropdownMenu.Group>
+                                <DropdownMenu.Label>Factory</DropdownMenu.Label>
+                                <DropdownMenu.Separator />
+                                <DropdownMenu.Item
+                                    onSelect={() => {
+                                        factory.paused = !factory.paused;
+                                    }}
+                                    >{factory.paused ? "Resume" : "Pause"} production</DropdownMenu.Item
+                                >
+                                <DropdownMenu.Item
+                                    onSelect={() => {
+                                        $gameState.factories.splice(index, 1);
+                                        $gameState.money += Math.round(
+                                            factory.purchaseData.value *
+                                                FACTORY_CONSTANTS.sellPriceMultiplier,
+                                        );
+                                    }}
+                                    >Sell for ${Math.round(
                                         factory.purchaseData.value *
                                             FACTORY_CONSTANTS.sellPriceMultiplier,
-                                    );
-                                }}
-                                >Sell for ${Math.round(
-                                    factory.purchaseData.value *
-                                        FACTORY_CONSTANTS.sellPriceMultiplier,
-                                )})</DropdownMenu.Item
-                            >
-                        </DropdownMenu.Group>
-                    </DropdownMenu.Content>
-                </DropdownMenu.Root>
-            </div>
-            <div class="grid grid-cols-2 gap-2">
+                                    )})</DropdownMenu.Item
+                                >
+                            </DropdownMenu.Group>
+                        </DropdownMenu.Content>
+                    </DropdownMenu.Root>
+                </div>
+            </Card.Header>
+            <Separator />
+            <Card.Content class="grid grid-cols-2 gap-2">
                 <div>
                     Cost: ${factory.purchaseData.cost}
                 </div>
@@ -103,7 +107,7 @@
                 <div>
                     {getProductionPerDay(factory)} produced per day
                 </div>
-            </div>
-        </div>
+            </Card.Content>
+        </Card.Root>
     {/each}
 </div>
