7b84d10619dfb7e6f0f34e9c5531ea9a0f04673b
Ingo Wolf <me@ingowolf.au>
2025-10-05 12:23:56 +1100

    add gradient

--- src/routes/analytics/+page.svelte
+++ src/routes/analytics/+page.svelte
@@ -2,7 +2,7 @@
     import { gameState } from "$lib/state.svelte";
 
     import * as Chart from "$lib/components/ui/chart/index.js";
-    import { LineChart } from "layerchart";
+    import { Area, AreaChart, LinearGradient, LineChart } from "layerchart";
 </script>
 
 <h2 class="text-2xl">Analytics</h2>
@@ -15,13 +15,29 @@
         },
     }}
 >
-    <LineChart
+    <AreaChart
         data={$gameState.analytics.moneyPerDay?.map((money, index) => ({
             money: money,
             day: index + 1,
         }))}
         x="day"
         series={[{ key: "money", color: "blue", label: "Money" }]}
-    />
+    >
+        {#snippet marks({ series, getAreaProps })}
+            {#each series as s, i (s.key)}
+                <LinearGradient
+                    stops={[
+                        s.color ?? "",
+                        "color-mix(in lch, " + s.color + " 10%, transparent)",
+                    ]}
+                    vertical
+                >
+                    {#snippet children({ gradient })}
+                        <Area {...getAreaProps(s, i)} fill={gradient} />
+                    {/snippet}
+                </LinearGradient>
+            {/each}
+        {/snippet}
+    </AreaChart>
 </Chart.Container>
 {$gameState.analytics.moneyPerDay}
