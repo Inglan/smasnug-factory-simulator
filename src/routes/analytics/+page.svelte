76bff09114dfcf3afc90e5f37cbbf32306c68de7
Ingo Wolf <me@ingowolf.au>
2025-10-05 12:21:05 +1100

    add money chart

--- src/routes/analytics/+page.svelte
+++ src/routes/analytics/+page.svelte
@@ -1,7 +1,20 @@
 <script lang="ts">
     import { gameState } from "$lib/state.svelte";
+
+    import * as Chart from "$lib/components/ui/chart/index.js";
+    import { LineChart } from "layerchart";
 </script>
 
 <h2 class="text-2xl">Analytics</h2>
 
+<Chart.Container config={{}}>
+    <LineChart
+        data={$gameState.analytics.moneyPerDay?.map((money, index) => ({
+            money: money,
+            day: index + 1,
+        }))}
+        x="day"
+        series={[{ key: "money", color: "blue", label: "Money" }]}
+    />
+</Chart.Container>
 {$gameState.analytics.moneyPerDay}
